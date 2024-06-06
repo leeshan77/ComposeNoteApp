b4428502310cb975b54b64e8b39ff98e554062c8
KOLEE <leeshan77@naver.com>
2024-06-07 03:37:14 +0900

    template

---
+++ app/src/main/java/com/kolee/composenoteapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.kolee.composenoteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.kolee.composenoteapp.ui.theme.ComposeNoteAppTheme
+import com.kolee.navigation.SetupNavigation
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SetupNavigation(
+                        navController = rememberNavController()
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/kolee/composenoteapp/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.kolee.composenoteapp.utils
+
+import androidx.compose.ui.graphics.Color
+
+object Constants {
+
+    const val NOTES_COLLECTION_REF = "NOTES"
+
+    val colors = listOf(
+        Color(0xFFFFFF8D).copy(alpha = 0.76f),
+        Color(0xFFFFCEBF).copy(alpha = 0.76f),
+        Color(0xFF84FFFF).copy(alpha = 0.76f)
+    )
+}
